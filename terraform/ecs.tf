96a9101eeb804bf311e5503ace78296b0816ab3d
Sander Van Grunderbeeck <sander@dataroots.io>
2021-04-13 18:33:42 +0200

    container test

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,7 +1,3 @@
-resource "aws_cloudwatch_log_group" "dagster" {
-  name              = "dagster"
-  retention_in_days = 1
-}
 
 resource "aws_ecs_cluster" "dagster" {
   name               = "dagster-cluster"
@@ -28,20 +24,12 @@ resource "aws_ecs_task_definition" "dagster" {
         "image": "mikesir87/aws-cli",
         "name": "sidecar_container",
         "command": [
-            "/bin/bash -c \"aws s3 cp ${var.file_path} ${var.dagster-container-home} --recursive"
+            "/bin/bash -c \"aws s3 cp ${var.file_path} ${var.dagster-container-home} --recursive\""
         ],
         "entryPoint": [
             "sh",
             "-c"
         ],
-        "logConfiguration": {
-            "logDriver": "awslogs",
-            "options": {
-              "awslogs-group": "${aws_cloudwatch_log_group.dagster.name}",
-              "awslogs-region": "eu-west-1",
-              "awslogs-stream-prefix": "airflow"
-          }
-        },
         "essential": true,
         "mountPoints": [
           {
