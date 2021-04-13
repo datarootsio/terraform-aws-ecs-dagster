3edcbe8975688be5565928deed11348022bb08fd
Sander Van Grunderbeeck <sander@dataroots.io>
2021-04-13 18:54:39 +0200

    test container

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "dagster-first-buckett"
-  acl    = "public-read-write"
-
-  versioning {
-    enabled = true
-  }
-}
+//resource "aws_s3_bucket" "b" {
+//  bucket = "dagster-first-buckett"
+//  acl    = "public-read-write"
+//
+//  versioning {
+//    enabled = true
+//  }
+//}
 
