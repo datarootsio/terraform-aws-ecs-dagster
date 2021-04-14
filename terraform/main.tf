3a567ed8774b83beddbf6117e1e6aba2920301db
paololeonard <paolo@dataroots.io>
2021-04-14 11:01:22 +0200

    added main file

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  version = "~> 3.12.0"
+  region  = var.aws_region
+}
