bb8b60b4ae4d78193e95787a23db5179ddd54d23
paololeonard <paolo@dataroots.io>
2021-04-19 09:30:04 +0200

    some cleaning

--- terraform/files/tests/simple_dagster/seed_pipeline.py
+++ terraform/files/tests/simple_dagster/seed_pipeline.py
@@ -15,32 +15,6 @@ def syncing_pipeline():
     s3_bucket()
 
 
-@solid
-def show_it(context):
-    # Remove hardcoded
-    cmd = 'ls /opt'
-    rc = run(args=cmd, shell=True, capture_output=True)
-    context.log.info(f"Showing result: {rc}")
-
-@pipeline
-def showit_pipeline():
-    show_it()
-
-
-@solid
-def print_it(context):
-    # Remove hardcoded
-    cmd = 'cat /opt/serial_pipeline.py'
-    rc = run(args=cmd, shell=True, capture_output=True)
-    context.log.info(f"printing result: {rc}")
-
-
-@pipeline
-def printit_pipeline():
-    print_it()
-
-
-
 @repository
 def syncing_pipelines():
-    return [syncing_pipeline, printit_pipeline, showit_pipeline]
+    return [syncing_pipeline]
